d1bbf2613c46b97381ca83deba99f2d26908d623
Sudha Rani <sudha.rani.r@sap.com>
2017-03-24 11:21:27 +0530

    Revert "Basic Configuration"
    This reverts commit b0b0c3fb9065670ad18c8cb8e7f3b7c3dce7a1c0.

--- src/main/java/com/elearning/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elearning.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.elearning.model.User;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
- 
-}
